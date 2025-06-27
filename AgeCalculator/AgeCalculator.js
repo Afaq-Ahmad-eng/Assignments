3b2cf1e8bc2a347726cf4c17671c9fd1efb8974e
Afaq-Ahmad-eng <afaqahmad2593@gmail.com>
2025-06-27 23:01:26 +0500

    We calculate the year successfully means find how we old are in years correctly

--- AgeCalculator/AgeCalculator.js
+++ AgeCalculator/AgeCalculator.js
@@ -1,16 +1,25 @@
 // We create calculate variable in this we will store the user birth day date
 const calculate = document.getElementById("Calculate");
 
+// we create a function to handle the calculation
+function handleCalculation() {
+    const birthDay = document.getElementById("dateOfBirthInput").value;
+    const todayDate = new Date();
+    const birthDayOfUser = new Date(birthDay);
+
+    // From Here we got the Birth Day year
+  let yearOfBirthDay = todayDate.getFullYear() - birthDayOfUser.getFullYear();
+
+//   From here we got Birth day Month
+ const monthOfBirthDay = (todayDate.getMonth() + 1) - (birthDayOfUser.getMonth() + 1);
+
+  console.log(`Current Month =  ${todayDate.getMonth() + 1}`);
+  console.log(`Birth day Month =  ${birthDayOfUser.getMonth() + 1}`);
+  console.log(`Year =  ${yearOfBirthDay = (monthOfBirthDay < birthDayOfUser.getMonth() + 1 ? yearOfBirthDay - 1 : yearOfBirthDay)}`);
+}
+
 // We create a Event Listener to lisen when the calculate button is click
 calculate.addEventListener("click", (e) => {
   e.preventDefault();
-  const birthDay = document.getElementById("dateOfBirthInput").value;
-
-  const todayDate = new Date();
-  const birthDayOfUser = new Date(birthDay);
-  const age = todayDate.getFullYear() - birthDayOfUser.getFullYear();
-
-  console.log(`Birth Day Date ${birthDay}`);
-  console.log(`Today Date ${todayDate.getFullYear()}`);
-  console.log(`Age =  ${age}`);
+  handleCalculation();
 });
